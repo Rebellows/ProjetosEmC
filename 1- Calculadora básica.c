1682e7168faeb53bfeae21e95f04b2557fbc7f50
Rebellows <130177863+Rebellows@users.noreply.github.com>
2023-05-02 11:00:16 -0300

    Update 1- Calculadora básica.c

--- 1- Calculadora básica.c
+++ 1- Calculadora básica.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <math.h>
 
 char nome[99];
 char c;
@@ -9,10 +10,10 @@ int valor, num, v = 0, produto = 1, soma = 0, sub = 0, divi = 0;
 int main() {
     printf("Digite seu nome: ");
     scanf("%s", nome);
-    printf("Olá %s, o que deseja fazer?\n [1] Calculadora\n [2] Benção\n Digite aqui: ", nome);
+    printf("Olá %s, o que deseja fazer?\n [1] Calculadora\n [2] Benção\nDigite aqui: ", nome);
     scanf("%d", &valor);
     if (valor == 1) {
-        printf("Certo, você deseja usar a calculadora. Qual das opções deseja fazer?\n [1] Soma\n [2] Subtração\n [3] Divisão\n [4] Multiplicação\n Digite aqui: ");
+        printf("Certo, você deseja usar a calculadora. Qual das opções deseja fazer?\n [1] Adição        | [7]\n [2] Subtração     | [8]\n [3] Divisão       | [9]\n [4] Multiplicação | [10]\n [5] Potenciação   | [11]\n [6] Radiciação    | [12]\nDigite aqui: ");
         scanf("%d", &num);
         if (num == 4) {
             printf("Entendido, vamos multiplicar.\n");
@@ -76,11 +77,38 @@ int main() {
             divi /= v;          
             }
             printf("O resultado obtido ao efetuar a divisão é: %d", divi); 
-            return 0; 
+            return 0;
         }
-    }
+        if (num == 5) {
+            printf("Entendido, faremos potenciação!\n");
+            float a, b, c;
+            printf("Digite um número a ser elevado: ");
+            scanf("%f", &a);
+            printf("Digite o seu expoente: ");
+            scanf("%f", &b);
+            c = pow(a, b);
+            printf("O resultado de %.1f^%.1f é aproximadamente: %.1f", a, b, c);
+            }
+        if (num == 6) {
+            float b, i, r;
+            printf("Certo, vamos calcular raízes!\n");
+            printf("Digite o valor a aplicar a raiz: ");
+            scanf("%f", &b);
+            printf("Digite o índice da raiz: ");
+            scanf("%f", &i);
+            r = pow(b, 1/i);
+            printf("O valor de %.1f em uma raíz de índice %.1f é aproximadamente: %.1f", b, i, r);
+            return 0;
+            }
+        else {
+            printf("O valor digitado não é válido.");
+            }
+        }
     if (valor == 2) {
         printf("%s, Deus te abençoe.", nome);
         return 0;
+        }
+    else if (valor != 1 && valor != 2) {
+        printf("O valor digitado não é válido.");
+        }
     }
-}
