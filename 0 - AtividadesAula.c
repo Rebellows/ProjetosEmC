136a22d7f974d937b05c9dd998ed8212a6ca079b
Rebellows <130177863+Rebellows@users.noreply.github.com>
2023-05-09 10:15:54 -0300

    Update 0 - AtividadesAula.c

--- 0 - AtividadesAula.c
+++ 0 - AtividadesAula.c
@@ -17,3 +17,27 @@ int main() {
     troca(&a, &b);
     printf("Valor a trocado = %d, valor b trocado = %d", a, b);
 }
+/////
+#include <stdio.h>
+#include <stdlib.h>
+
+int auxa(int n) {
+    return n / 2;
+}
+int auxb(int n) {
+    return n * 3 + 1;
+}
+int main() {
+    int n, a, b;
+    printf("Escreva um número inteiro: ");
+    scanf("%d", &n);
+    if (n % 2 == 0) {
+        a = auxa(n);
+        printf("Valor novo: %d", a);
+    }
+    if (n % 2 != 0) {
+        b = auxb(n);
+        printf("Valor novo: %d", b);
+    }
+    return 0;
+}
