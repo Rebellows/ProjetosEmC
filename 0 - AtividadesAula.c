ed9a703d2e34bc30d4d1ecc85ad98b60174a4c01
Rebellows <130177863+Rebellows@users.noreply.github.com>
2023-05-09 18:39:19 -0300

    Update 0 - AtividadesAula.c

--- 0 - AtividadesAula.c
+++ 0 - AtividadesAula.c
@@ -112,3 +112,22 @@ int main () {
     printf("\nO resultado é: %d", c);
     return 0;
 }
+///// mostra as casas do número
+#include <stdio.h>
+#include <stdlib.h>
+
+void aux(int a, int *milhar, int *centena, int *dezena, int *unidade) {
+    *milhar = a / 1000;
+    *centena = (a / 100) % 10;
+    *dezena = (a / 10) % 10;
+    *unidade = a % 10;
+}
+
+int main() {
+    int a, milhar, centena, dezena, unidade;
+    printf("Digite um número inteiro de quatro dígitos: ");
+    scanf("%d", &a);
+    aux(a, &milhar, &centena, &dezena, &unidade);
+    printf("Milhar: %d\nCentena: %d\nDezena: %d\nUnidade: %d\n", milhar, centena, dezena, unidade);
+    return 0;
+}
