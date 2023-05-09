2316262518818408bd6bf07a8385185e034c4763
Rebellows <130177863+Rebellows@users.noreply.github.com>
2023-05-09 10:39:13 -0300

    Update 0 - AtividadesAula.c

--- 0 - AtividadesAula.c
+++ 0 - AtividadesAula.c
@@ -41,3 +41,20 @@ int main() {
     }
     return 0;
 }
+/////
+#include <stdio.h>
+#include <stdlib.h>
+
+int hm(int minutos, int *h, int *m) {
+    *m = minutos % 60;
+    int temp = minutos - *m;
+    *h = temp / 60;
+}
+
+int main() {
+    int minutos, h, m, a;
+    printf("Digite os minutos: ");
+    scanf("%d", &minutos);
+    a = hm(minutos, &h, &m);
+    printf("%dh%dm", h, m);
+}
