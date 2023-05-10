0c4d290893049ce2ebc892d7f96a1a54dd2620f9
Rebellows <130177863+Rebellows@users.noreply.github.com>
2023-05-10 19:28:09 -0300

    Update 0 - AtividadesAula.c

--- 0 - AtividadesAula.c
+++ 0 - AtividadesAula.c
@@ -170,3 +170,42 @@ int main() {
         }
     }
 }
+///// calcula a área de um triangulo se houver
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int valor(int lado_um, int lado_dois, int lado_tres) {
+    if (lado_um >= lado_dois + lado_tres || lado_dois >= lado_um + lado_tres || lado_tres >= lado_um + lado_dois) {
+        return 0;
+    }
+    else {
+        return 1;
+    }
+}
+
+int aux(int lado_um, int lado_dois, int lado_tres) {    
+    int sp = ((lado_um + lado_dois + lado_tres)/2);
+    int area = sqrt(sp * (sp - lado_um) * (sp - lado_dois) * (sp - lado_tres));
+    return area;
+}
+
+int main() {
+    int lado_um, lado_dois, lado_tres, a, b;
+    printf("Digite o primeiro lado: ");
+    scanf("%d", &lado_um);
+    printf("Digite o segundo lado: ");
+    scanf("%d", &lado_dois);
+    printf("Digite o terceiro lado: ");
+    scanf("%d", &lado_tres);
+    a = valor(lado_um, lado_dois, lado_tres);
+    if (a == 0) {
+        printf("Essa porra não é um triângulo.");
+        return 0;
+    }
+    else {
+        b = aux(lado_um, lado_dois, lado_tres);
+        printf("A área é %d", b);
+        return 0;
+    }
+}
