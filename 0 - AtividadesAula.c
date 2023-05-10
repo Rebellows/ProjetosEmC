33c55bdf406ed80a9cede5091dcc15cb2159db66
Rebellows <130177863+Rebellows@users.noreply.github.com>
2023-05-10 17:32:51 -0300

    Update 0 - AtividadesAula.c

--- 0 - AtividadesAula.c
+++ 0 - AtividadesAula.c
@@ -131,3 +131,42 @@ int main() {
     printf("Milhar: %d\nCentena: %d\nDezena: %d\nUnidade: %d\n", milhar, centena, dezena, unidade);
     return 0;
 }
+///// verifica se tal numero é primo
+#include <stdio.h>
+#include <stdlib.h>
+
+int aux(int n) {
+    int c, r;
+    for (c = 1; c <= n; c++) {
+        if (n % c == 0) {
+            r++;
+        }
+    }
+    if (r == 2) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+int main() {
+    int num, a;
+    printf("Digite um número inteiro positivo: ");
+    scanf("%d", &num);
+    if (num < 0) {
+        printf("Número inválido.");
+        return 0;
+    }
+    else {
+        a = aux(num);
+        if (a == 1) {
+            printf("É primo.");
+            return 0;
+        }
+        else {
+            printf("Não é primo.");
+            return 0;
+        }
+    }
+}
