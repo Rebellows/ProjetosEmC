b843fed7023c6820b60401da98b6226e6cc5bf4b
Rebellows <130177863+Rebellows@users.noreply.github.com>
2023-05-23 10:28:52 -0300

    Update 0 - AtividadesAula.c

--- 0 - AtividadesAula.c
+++ 0 - AtividadesAula.c
@@ -226,3 +226,23 @@ int main() {
     printf("\n");
     return 0;
 }
+///// conta a quantidade de caracteres de uma palavra
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int comprimento_string(char str[]) {
+    int tamanho = 0;
+    while(str[tamanho] != '\0') {
+        tamanho++;
+    }
+    printf("Essa palavra tem %d caracteres.", tamanho);
+}
+
+int main() {
+    char str[100];
+    printf("Digite algo: ");
+    scanf("%s", str);
+    comprimento_string(str);
+    return 0;
+}
