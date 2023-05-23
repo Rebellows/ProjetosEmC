daf6b96a8261fa245f412bf451d78710f0c4c2cf
Rebellows <130177863+Rebellows@users.noreply.github.com>
2023-05-23 10:09:46 -0300

    Update 0 - AtividadesAula.c

--- 0 - AtividadesAula.c
+++ 0 - AtividadesAula.c
@@ -209,3 +209,20 @@ int main() {
         return 0;
     }
 }
+///// inverte um nome
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+    char nome[99];
+    int i;
+    printf("Digite um nome: ");
+    scanf("%s", nome);
+    printf("O nome ao contrário é");
+    for (i = strlen (nome) - 1; i >= 0; i--) {
+        printf("%c", nome[i]);
+    }
+    printf("\n");
+    return 0;
+}
